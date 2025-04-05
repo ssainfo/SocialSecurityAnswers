9e2613da1d6de8decefd8e7ec45e48262209a378
ssainfo <ravellc17@gmail.com>
2025-04-05 14:26:13 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-console.log("Script.js loaded successfully");
 document.addEventListener("DOMContentLoaded", function () {
-    const pages = [
+    console.log("DOMContentLoaded triggered successfully");
+});    const pages = [
         { url: "/", title: "Home" },
         { url: "/news.html", title: "News" },
         { url: "/replacement.html", title: "Card Replacement" },
